439bd65f6daad2bc77bf26a16d2a9fb06f83b388
ply <710729301@qq.com>
2022-06-08 11:13:14 +0800

    m

--- auto_oa/pg.py
+++ auto_oa/pg.py
@@ -8,12 +8,40 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from faker import Faker
+fake = Faker(['zh_CN'])
+
+# Faker.seed(0)
+# for _ in range(5):
+#     name = fake.name()
+#     address = fake.address()
+#     date = fake.date_this_month() # date_this_year() date_this_decade()
+#     company = fake.company()
+#     mobile = fake.phone_number()
+#     char = fake.sentence(nb_words=10, variable_nb_words=True, ext_word_list=None).replace('.', '')
+#     text = fake.paragraph(nb_sentences=5)
+#     print(name)
+#     print(address)
+#     print(date)
+#     print(company)
+#     print(mobile)
+#     print(char)
+#     print(text)
+
+
+
 # The below two lines fixed USB error logging
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_experimental_option("detach", True)
 
-PATH = 'C:\Program Files (x86)\chromedriver.exe'
-driver = webdriver.Chrome(PATH, options=options)
+# PATH = 'C:\Program Files (x86)\chromedriver.exe'
+PATH = './chromedriver.exe'
+# driver = webdriver.Chrome(PATH, options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 pg_url = url + '?next=/pg/pg_list/'
 driver.get(pg_url)
@@ -23,11 +51,13 @@ login = driver.find_element(By.CSS_SELECTOR, 'button[type=submit]')
 
 username.send_keys("user63")
 password.send_keys("pass63")
-login.click()
+# login.click()
+webdriver.ActionChains(driver).move_to_element(login).click(login).perform()
 
 
 create_button = driver.find_element(By.CSS_SELECTOR, '#card_add')
-create_button.click()
+# create_button.click()
+webdriver.ActionChains(driver).move_to_element(create_button).click(create_button).perform()
 
 driver.find_element(By.CSS_SELECTOR, 'input[name=client]').send_keys('世界人民银行苏州分行')
 driver.find_element(By.CSS_SELECTOR, 'input[name=contact_person]').send_keys('关云长')
@@ -61,7 +91,7 @@ driver.execute_script("let x= document.getElementById('appraise_time');"+"x.valu
 
 
     
-    
+
 
 # WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, 'input[name=appraise_time]')))
 # driver.close()
--- watermark/main.py
+++ watermark/main.py
@@ -4,32 +4,34 @@ from pathlib import Path
 
 # base_dir = os.getcwd()  # for running in the console
 base_dir = os.path.dirname(os.getcwd()) # for running executable file
+base_dir = base_dir + "/watermark"
 input_path = base_dir + '\inputs'
 out_path = base_dir + '\outputs'
 logo = base_dir + '\logo.png'
 
 
-logo = Image.open(logo)
-# logo = Image.open("C:\\Users\\pengliangyu\\Desktop\\utils\\watermark\\logo.png")
+def resize_watermark(input_path):
+    logo = Image.open(logo)
+    # logo = Image.open("C:\\Users\\pengliangyu\\Desktop\\utils\\watermark\\logo.png")
+    # resize the logo
+    lwidth, lheight = logo.size
+    logo = logo.resize((int(lwidth*2), int(lheight*2)))
+    width_of_watermark , height_of_watermark = logo.size
 
-# resize the logo
-lwidth, lheight = logo.size
-logo = logo.resize((int(lwidth*2), int(lheight*2)))
-width_of_watermark , height_of_watermark = logo.size
+    for filename in os.listdir(input_path):
+        f = os.path.join(input_path, filename)
+        if os.path.isfile(f):
+            im = Image.open(f)
+            if im.width > 1000 or im.height > 1000:
+                im = ImageOps.contain(im, (1000, 1000))
+            width, height = im.size
 
-for filename in os.listdir(input_path):
-    f = os.path.join(input_path, filename)
-    if os.path.isfile(f):
-        im = Image.open(f)
-        if im.width > 1000 or im.height > 1000:
-            im = ImageOps.contain(im, (1000, 1000))
-        width, height = im.size
+            position = (int((width/2-width_of_watermark/2)),int((height/2-height_of_watermark/2)))
 
-        position = (int((width/2-width_of_watermark/2)),int((height/2-height_of_watermark/2)))
+            copied_image = im.copy()
+            copied_image.paste(logo.copy(), position, logo)
+            copied_image.save(os.path.join(out_path, filename))
+    print('Mission Accomplished!')
 
-        copied_image = im.copy()
-        copied_image.paste(logo.copy(), position, logo)
-        copied_image.save(os.path.join(out_path, filename))
 
-print('everything is ok') 
-
+for estate_name in os.listdir(base_dir)
